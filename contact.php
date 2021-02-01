6f86e786c0861732513690b6265ccd752197bc07
j-spinelli <59508674+j-spinelli@users.noreply.github.com>
2021-02-01 18:32:37 -0300

    Background
    Le agregué un background a la pagina.

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include("php/connection.php"); ?>
+<html>
+<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="scripts/main-scripts.js"></script>
+		<title>Contáctenos</title>
+		
+	</head>
+	<body class= "ContactBody" style="background-image: url(pictures/manuel-castanon-the-hopeful-knight-final.jpg);">
+		<?php include("menu.php") ?>
+	</body>
+	</html>
